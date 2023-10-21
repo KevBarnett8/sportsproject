ca8b8c86824e89cbdfad5042730a4a8439d10719
Kevin <Kevbarnett88@gmail.com>
2023-10-21 14:21:56 -0400

    First Commit

---
+++ src/main/java/com/thesportsacademy/sportsproject/SportsprojectApplication.java
@@ -0,0 +1,13 @@
+package com.thesportsacademy.sportsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thesportsacademy/sportsproject/SportsprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thesportsacademy.sportsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
